f500ef7408c76ac037ad0033376c90172fcb4c12
MA <62049047+yumawen@users.noreply.github.com>
2020-05-08 23:08:53 +0800

    Add files via upload

---
+++ A.py
@@ -0,0 +1,8 @@
+#求余数%，  求商//
+# sum=[1,2,3]
+# print(sum[0:2])
+
+sum=[{'obj':5},2,3]
+sum.insert(0,4)
+
+print(sum[1]['obj'])
---
+++ index.py
@@ -0,0 +1,30 @@
+# l = 1
+# ma = globals()
+# wen = locals()
+#print(dict())
+
+'''ma=1
+print(ma)
+ma_type=type(ma)
+print(ma_type )
+'''
+
+'''name="mawen"
+sex='男'
+print(name,sex,sep=':')
+print('name\nsex')
+print(name, end='我儿子')
+'''
+
+''''product='name:%s id:%d price:%f'%('快乐水',1,5.5)
+print(product)
+pi=3.14
+print('圆周率:%.2f'%pi,type(pi))
+student_id=2
+print('学号:%03d'%student_id)
+#03学号不够三位在前面补0
+'''
+
+#input接收的数据,result的数据一定是str
+result=input('请输入一个数据')
+print('接受的数据是:',result,'接受的数据类型:',type(result))
